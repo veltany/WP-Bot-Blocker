b8fc92662f4f222feb31a3441eca9915bb181e71
Engr Sam Chuks <sammychukwu4real@yahoo.com>
2024-11-08 13:17:32 +0100

    Update class-wp-bot-blocker-helper.php
    Added helper method cache_getresult() to cache WordPress database query [get_results()]

--- includes/class-wp-bot-blocker-helper.php
+++ includes/class-wp-bot-blocker-helper.php
@@ -3,7 +3,36 @@ if (!defined('ABSPATH')) exit;
 
 class WP_Bot_Blocker_Helper {
 
-    public  function log($message) {
+/*
+* Mofified wordpress get_results() 
+* function to include Caching the result
+*  where applicable 
+*/
+public function cache_getresult($query, $cache_key )
+{
+  //prefix to avoid conflicts 
+  $cache_key = 'wp_bot_blocker'.$cache_key;
+  // find existing cache
+   $cache = wp_cache_get($cache_key);
+   if($cache)
+   {
+     $result = $cache;
+   }
+   else 
+   {
+      global $wpdb;
+      $result = $wpdb->get_results($query);
+      if ( $wpdb->last_error ) 
+      {
+          return false;
+      }
+   } 
+ 
+ return $result ;
+}
+
+
+public  function log($message) {
         
             $log_file = plugin_dir_path(__FILE__) . '../logs/logs.txt';
             $time = gmdate('Y-m-d H:i:s');
